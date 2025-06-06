cfe6184a4830a9ebe478923cfb6502276e76810e
Natyellen F. <natyellenfranca8@gmail.com>
2025-06-06 17:21:35 -0300

    add de sistema

---
+++ Sistema-EntradaSaida/app/main/assets/config/Database.php
@@ -0,0 +1,33 @@
+<?php
+//CONEXÃO COM O BANCO
+class Database {
+    private $dsn = 'mysql:host=localhost;dbname=entrada e saída neww';
+    private $username = 'root';
+    private $password = '';
+    private $connection = null;
+
+    public function connect() {
+        try {
+            $this->connection = new PDO($this->dsn, $this->username, $this->password);
+          
+          $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->connection;
+        } catch (PDOException $exception) {
+            throw new Exception("Connection error: " . $exception->getMessage());
+        }
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+
+    public function closeConnection() {
+        $this->connection = null;
+    }
+    
+};
+
+
+
+
+?>
